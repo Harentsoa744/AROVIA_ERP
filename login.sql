4e5243ab9fd899bd77ab845498caba2c249a6da8
Harinala <arnalarab@gmail.com>
2026-07-14 17:21:35 +0400

    checkbox employe,livraison

--- login.sql
+++
@@ -1,3 +0,0 @@
-('Raza',   'Andry', 'compta@arovia.com',  '$2y$10$changeme', 2),  -- COMPTABLE
-('Rakoto', 'Jean',  'magasin@arovia.com', '$2y$10$changeme', 3),  -- MAGASINIER
-('Randria','Marc',  'livreur1@arovia.com','$2y$10$changeme', 4);  -- LIVREUR
